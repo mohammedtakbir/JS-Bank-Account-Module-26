d8cbd4300ddfc3921c341e2a26a6f29d5f2bc462
mohammedtakbir <takbirhossain678@gmail.com>
2022-08-17 19:33:00 +0600

    validation added

--- js/withdraw.js
+++ js/withdraw.js
@@ -6,28 +6,46 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const withdrawFieldString = withdrawField.value;
     const withdrawAmount = parseFloat(withdrawFieldString);
 
+     // * step-7. clear the withdraw field
+     withdrawField.value ='';
+
+    // ! Validation
+    if(isNaN(withdrawAmount)){
+        alert('please enter number');
+        return;
+    }
+
     // * step-3. get the current total withdraw amount
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const withdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(withdrawTotalString);
 
-    // * step-4. add numbers to set the total withdraw
-    const currentTotalWithdraw = previousWithdrawTotal + withdrawAmount;
-    // * set the withdraw total
-    withdrawTotalElement.innerText = currentTotalWithdraw;
     
     // * step-5. get balance current total
     const balanceTotal = document.getElementById('balance-total');
     const PreviousBalaneTotalString = balanceTotal.innerText;
     const PreviousBalaneTotal = parseFloat(PreviousBalaneTotalString);
 
+    
+
+    // ! Validaitonn
+    if(withdrawAmount > PreviousBalaneTotal){
+        alert('You have not enough money');
+        return;
+    }
+
+    // * step-4. add numbers to set the total withdraw
+    const currentTotalWithdraw = previousWithdrawTotal + withdrawAmount;
+    // * set the withdraw total
+    withdrawTotalElement.innerText = currentTotalWithdraw;
+    
+
     // * step-6. calculate current total balance
     const currentBalanceTotal2 = PreviousBalaneTotal - withdrawAmount;
     // * set the balance total
     balanceTotal.innerText = currentBalanceTotal2;
 
-     // * step-7. clear the withdraw field
-     withdrawField.value ='';
+    
 
 
 
