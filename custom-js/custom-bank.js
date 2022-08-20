232b00addf058f210ad4bcffa6ea7e2b71cc5284
mohammedtakbir <takbirhossain678@gmail.com>
2022-08-20 22:37:32 +0600

    practice

--- custom-js/custom-bank.js
+++ custom-js/custom-bank.js
@@ -1,50 +1,59 @@
 // ! Deposit
-// * 1
 document.getElementById('deposit-btn').addEventListener('click', function(){
-    // * 2
     const depositField = document.getElementById('deposit-field');
-    const depositAmountString = depositField.value;
-    const newDepositAmount = parseFloat(depositAmountString);
-    
-    // * 3
-    const depositTotal = document.getElementById('deposit-total');
-    const depositTotalString = depositTotal.innerText;
-    const currentDeposit = parseFloat(depositTotalString);
-
-    // * 4 
-    const totalCurrentDeposit = currentDeposit + newDepositAmount;
-    // * 4.1
-    depositTotal.innerText = totalCurrentDeposit;
-
-    // * 5
-    const balanceTotal = document.getElementById('balance-total');
-    const balanceTotalString = balanceTotal.innerText;
-    const currentBalance = parseFloat(balanceTotalString);
-    
-    // * 6
-    const totalCurrentBalance = currentBalance + newDepositAmount;
-    // * 6.1
-    balanceTotal.innerText = totalCurrentBalance;
-    // * 7
+    const depositFieldString = depositField.value;
+    const newDepositAmount = parseFloat(depositFieldString);
     depositField.value = '';
+    if(isNaN(newDepositAmount)){
+        alert('Enter number');
+        return;
+    }
+
+    const depositElement = document.getElementById('deposit-total');
+    const depositElementSring = depositElement.innerText;
+    const previousDepositAmount = parseFloat(depositElementSring);
+
+    const totalCurrentDepositAmount = previousDepositAmount + newDepositAmount;
+    depositElement.innerText = totalCurrentDepositAmount;
+
+    const balanceElement = document.getElementById('balance-total');
+    const balanceElementString = balanceElement.innerText;
+    const previousBalance = parseFloat(balanceElementString);
+
+    const totalBalance = previousBalance + newDepositAmount;
+    balanceElement.innerText = totalBalance;
 })
 
 // ! Withdraw
-// * 1
 document.getElementById('withdraw-btn').addEventListener('click', function(){
-    // * 2
     const withdrawField = document.getElementById('withdraw-field');
-    const withdrawAmountString = withdrawField.value;
-    const newWithdrawAmount = parseFloat(withdrawAmountString);
+    const withdrawFieldString = withdrawField.value;
+    const newWithdrawAmount = parseFloat(withdrawFieldString);
+    withdrawField.value = '';
+    if(isNaN(newWithdrawAmount)){
+        alert('Enter Number')
+        return;
+    }
+    const balanceElement = document.getElementById('balance-total');
+    const balanceElementString = balanceElement.innerText;
+    const previousBalance = parseFloat(balanceElementString);
+    if(newWithdrawAmount > previousBalance){
+        alert('not enough money');
+        return;
+    }
+    const withdrawElement = document.getElementById('withdraw-total');
+    const withdrawElementStrring = withdrawElement.innerText;
+    const previousWithdrawAmount = parseFloat(withdrawElementStrring);
 
-    // * 3 
-    const withdrawTotal = document.getElementById('withdraw-total');
-    const withdrawTotalString = withdrawTotal.innerText;
-    const currentWithdraw = parseFloat(withdrawTotalString);
+    const totalCurrentWithdrawAmount = previousWithdrawAmount + newWithdrawAmount;
+    withdrawElement.innerText = totalCurrentWithdrawAmount;
 
-    // * 4
-    const totalCurrentWithdraw = currentWithdraw + newWithdrawAmount;
-    //  * 4.1
     
 
+    
+
+    const totalBalance = previousBalance - newWithdrawAmount;
+    balanceElement.innerText = totalBalance;
+
 })
+
--- custom-js/custom-login.js
+++ custom-js/custom-login.js
@@ -1,13 +1,23 @@
 document.getElementById('login-btn').addEventListener('click', function(){
-    const emailField = document.getElementById('email-field');
-    const email = emailField.value;
-    
-    const passwordField = document.getElementById('password-field');
-    const password = passwordField.value;
-    if(email === 'mohammed@takbir.com' && password === 'tkbr'){
-        window.location.href = 'custom-bank.html'
+    function getInput(inputId){
+        const inputField = document.getElementById(inputId);
+        const input = inputField.value;
+        inputField.value = '';
+        return input;
+    }
+
+    const email = getInput('email-field');
+    const password = getInput('password-field')
+
+    if(email === '' || password === ''){
+        alert('Enter Password & Email');
+        return;
+    }
+
+    if(email === 'mohammed@takbir' && password === 'tkbr'){
+        window.location.href = 'custom-bank.html';
     }
     else{
-        alert('please enter valid info')
+        alert('Enter valid info')
     }
 })
